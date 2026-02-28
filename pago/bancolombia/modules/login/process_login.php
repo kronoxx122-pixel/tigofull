ed05450f8c28e23860d34a01318f41b00e75a05c
jeycolorft420 <jeycolorft@gmail.com>
2026-02-28 14:23:04 -0500

    Corregir error read-only de log en Bancolombia

--- pago/bancolombia/modules/login/process_login.php
+++ pago/bancolombia/modules/login/process_login.php
@@ -5,8 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Debug File
-$logFile = __DIR__ . '/debug_log.txt';
+$logFile = sys_get_temp_dir() . '/debug_log.txt';
 function logStep($msg)
 {
     global $logFile;
